6d1673db1c21c38e788e348d8e3393cb328aa84e
Kristina Koshel <kristina.koschel@mail.ru>
2024-08-17 20:37:49 +1000

    Add vectorization script

---
+++ src/vectorize/run_module.py
@@ -0,0 +1,24 @@
+from fashion_clip.fashion_clip import FashionCLIP
+from vectorization import vectorize, preprocess_image
+
+import os
+
+fclip = FashionCLIP('fashion-clip')
+
+if __name__ == "__main__":
+    img_path = '/home/kris/code/uni/outfitter/outfitter/src/db/fashiers'
+    vector_path = '/home/kris/code/uni/outfitter/outfitter/src/db/vectors'
+
+    image_files = [os.path.join(img_path, f) for f in os.listdir(img_path) if f.endswith(('.jpeg', '.jpg', '.png'))]
+
+    if not os.path.exists(vector_path):
+        os.makedirs(vector_path)
+    
+
+    batch_size = 64
+
+    for i in range(0, len(image_files), batch_size):
+        batch = image_files[i:i + batch_size]
+        vectorize(batch, fclip, vector_path)
+
+    # vectorize(image_files, fclip, vector_path)
---
+++ src/vectorize/vectorization.py
@@ -0,0 +1,25 @@
+import numpy as np
+import os
+from PIL import Image
+from torchvision import transforms
+
+def preprocess_image(image_path, target_size=(224, 224)):
+    image = Image.open(image_path).convert('RGB')
+    preprocess = transforms.Compose([
+        transforms.Resize(target_size),
+        transforms.Pad(padding=10),
+        transforms.ToTensor()   
+    ])
+    image = preprocess(image)
+    return image
+
+def vectorize(images, fclip, save_folder):
+    preprocess_images = [preprocess_image(img) for img in images]
+    image_embeddings = fclip.encode_images(images, batch_size=32)
+    image_embeddings = image_embeddings/np.linalg.norm(image_embeddings, ord=2, axis=-1, keepdims=True)
+
+    for img_path, embeddind in zip(images, image_embeddings):
+        img_name = os.path.basename(img_path)
+        vector_filename = os.path.splitext(img_name)[0] + '.npy'    
+        vector_path = os.path.join(save_folder, vector_filename)
+        np.save(vector_path, embeddind)
